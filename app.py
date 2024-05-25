20d2a44c9e17afd70884e7dc7aa706b315b9f690
Edgar <doforce@pm.me>
2024-05-25 17:35:49 +0800

    use template and chain

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 import os
 import gradio as gr
 from langchain_groq import ChatGroq
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
-
-PROMPTS = [
-    ("Detect Language", """
-Respond to the user's input language. If the user's input is in [Language 1], respond in [Language 1]. If the user's input is in [Language 2], respond in [Language 2]. If the user's input is in [Language 3], respond in [Language 3], and so on. Continue to adapt the response language to match the user's input language for each subsequent interaction."
-Example:
-Respond to the user's input language. If the user's input is in Chinese, respond in Chinese. If the user's input is in Spanish, respond in Spanish. If the user's input is in English, respond in English. If the user's input is in French, respond in French, and so on. Continue to adapt the response language to match the user's input language for each subsequent interaction.
-""")
-]
-
-
-VERSION = "v1.1.1"
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import AIMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
 
 MAX_TOKENS = os.getenv("MAX_TOKENS")
 if MAX_TOKENS is None:
@@ -20,53 +11,57 @@ if MAX_TOKENS is None:
 
 MAX_TOKENS = int(MAX_TOKENS)
 
+VERSION = "v1.2.0"
+
+question_template = """
+Answer the following questions as best you can.
+Question: {input}
+Answer: Respond in the language which above question is using.
+"""
+
 GROQ_MODELS = [
     "llama3-70b-8192",
-    "llama3-8b-8192",
     "mixtral-8x7b-32768",
     "gemma-7b-it",
+    "llama3-8b-8192",
 ]
 
 
-async def predict(message, history, model_name, temperature, max_tokens, prompt, custom_prompt: str):
+async def predict(message, history, model_name: str, temperature, max_tokens):
     llm = ChatGroq(model_name=model_name, temperature=temperature,
                    streaming=True, max_tokens=max_tokens)
     langchain_history = []
-    if custom_prompt.strip() != "":
-        langchain_history.append(SystemMessage(content=custom_prompt))
-    else:
-        langchain_history.append(SystemMessage(content=prompt))
     for human, ai in history:
         langchain_history.append(HumanMessage(content=human))
         langchain_history.append(AIMessage(content=ai))
-    langchain_history.append(HumanMessage(content=message))
+    prompt_template = ChatPromptTemplate.from_messages(
+        langchain_history + [("human", question_template)])
 
-    message = ""
-    async for chunk in llm.astream(langchain_history):
-        message = message + chunk.content
-        yield message
+    chain = prompt_template | llm | StrOutputParser()
+    msg = ''
+    async for chunk in chain.astream({"input": message}):
+        msg = msg + chunk
+        yield msg
 
 
 demo = gr.ChatInterface(
     fn=predict,
-    description=f"A Gradio chatbot powered by GroqCloud {VERSION}",
+    description=f"A Gradio chatbot powered by GroqCloud and Langchain {
+        VERSION}",
     additional_inputs_accordion=gr.Accordion(
         label="Parameters", render=False, open=False),
     additional_inputs=[
         gr.Dropdown(label="Model", choices=GROQ_MODELS, value=GROQ_MODELS[0]),
         gr.Slider(label="Temperature", minimum=0.0, maximum=1.0, step=0.1,
                   value=0.6),
-        gr.Number(label="Max Tokens", value=2048,
-                  step=1024, minimum=1024, maximum=MAX_TOKENS),
-        gr.Dropdown(label="Prompt", choices=PROMPTS, value=PROMPTS[0][1]),
-        gr.TextArea(label="Custom prompt",
-                    placeholder="If leave it empty, use prompt above.")
+        gr.Number(label="Max Tokens", value=MAX_TOKENS,
+                  step=1024, minimum=1024, maximum=32768),
     ],
     examples=[
-        ["What is Decision Tree Regression?"],
+        ["What is Decision Tree Regression"],
         ["Wite a love story with about 10000 words."],
-        ["Why should I care about fast inference?"],
         ["如何配置 Nginx 多域名和 SSL"],
+        ["llm の事前トレーニングと微調整とは何ですか?またその違いは何ですか"],
     ],
     cache_examples=False,
     title="Qboot"
